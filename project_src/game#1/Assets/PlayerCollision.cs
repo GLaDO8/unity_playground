077c883fc42295211644c0081844051e662a4e13
XshreyasguptaX <shreyas.charming@gmail.com>
2019-02-02 18:08:10 +0530

    modified source

---
+++ project_src/game#1/Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public playermovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle!");
+            movement.enabled = false;
+        }
+
+    }
+
+}
